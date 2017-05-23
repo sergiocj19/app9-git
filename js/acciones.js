cd01d786ddee556910db857c5de5ea6014174cc6
sergioccj19 <sergiodevirgo@gmail.com>
2017-05-23 08:07:39 -0500

    3subida

--- js/acciones.js
+++ js/acciones.js
@@ -26,7 +26,7 @@ function getPosition(){
 	
 	var watchID=navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 	
-	function onSuccess(Position){
+	function onSuccess(position){
 		
 		alert('Latitude:'   +position.coords.latitude +'\n'+
 		      'Longitude:'   +position.coords.longitude +'\n'+
